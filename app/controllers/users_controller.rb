63fef03f2b922f518a586f347d15f814be3b659d
Jmingus <jmingus@austin.rr.com>
2015-01-29 12:12:18 -0600

    adding filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   # GET /users.json
   def index
     @users = User.all
+    @user = User.new
   end
 
   # GET /users/1
@@ -26,13 +27,16 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
+    if @user.save
+      respond_to do |format|
+        format.html { redirect_to root_path, notice: 'User was successfully created.' }
+        format.json { render json: @user, status: :created, location: @user }
+       end
+        else
+        respond_to do |format|
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
+
       end
     end
   end
